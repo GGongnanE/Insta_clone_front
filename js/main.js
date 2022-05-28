fa58414de66ba182d229979ff383da461752eb4e
GGNE <z_zzlzl@naver.com>
2022-05-28 22:33:50 +0900

    작업 내역 저장(ajax)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-const heart = document.querySelector('.heart_btn');
 const header = document.querySelector('#header');
 const sidebox = document.querySelector('.side_box');
 const variableWidth = document.querySelectorAll('.contents_box .contents');
-
+const delegation = document.querySelector('.contents_box');
 
 /**
  *  이벤트 위임
@@ -25,11 +24,68 @@ function delegationFunc(e) {
     if (element.matches('[data-name="heartbeat"]')) {
         console.log('하트비트~!');
 
+        // ajax (jquery) -> TODO : JS fetch 
+        $.ajax({
+            type: 'GET',
+            url: 'data/like.json',
+            data: 37,
+            dataType: 'json',
+            success: function(response) {
+                let likecount = document.querySelector('#like-count-37');
+                likecount.innerHTML = '좋아요 ' + response.like_count + '개';
+            },
+            error: function(request, status, error) {
+                // TODO : 에러 처리 구현 필요 
+                alter('로그인이 필요합니다.')
+                window.location.replace('https://www.naver.com');
+            }
+        });
     } else if (element.matches('[data-name="bookmark"]')) {
-        console.log('북마크');
+        
+        let pk = element.getAttribute('name');
 
-    }  else if (element.matches('[data-name="share"]')) {
-        console.log('공유');
+        $.ajax({
+            type: 'GET',
+            url: 'data/bookmark.json',
+            data: {pk},
+            dataType: 'json',
+            success: function(response) {
+                let bookmarkCount = document.querySelector('#bookmark-count-37');
+                bookmarkCount.innerHTML = '북마크 ' + response.bookmark_count + '개';
+            },
+            error: function(request, status, error) {
+                // TODO : 에러 처리 구현 필요 
+                alter('로그인이 필요합니다.')
+                window.location.replace('https://www.naver.com');
+            }
+        });
+
+    } else if (element.matches('[data-name="comment"]')) {
+        
+        let content = document.querySelector('#add-comment-post-37 > input[type=text]').value;
+        // console.log(content);
+
+        if (content.length > 200) {
+            alert('댓글은 최대 200자까지만 입력 가능합니다. 현재 글자수 : ' + content.length);
+            return;
+        }
+
+        $.ajax({
+            type: 'GET',
+            url: './comments.html',
+            data: {
+                'pk':37,
+                'content':content,
+            },
+            dataType: 'html',
+            success: function(data) {
+                document.querySelector('#comment-list-ajax-post37').insertAdjacentHTML('afterbegin', data);
+            },
+            error: function(request, status, error) {
+                // TODO : 에러 처리 구현 필요 
+                alter('에러가 발생했습니다. 관리자에게 문의하세요.')
+            }
+        });
 
     } else if (element.matches('[data-name="more"]')) {
         console.log('더보기');
@@ -65,18 +121,29 @@ function scrollFunc() {
 
     if (scrollY >= 10) {
         header.classList.add('on');
-        sidebox.classList.add('on');
+
+        if (sidebox) {
+            sidebox.classList.add('on');
+        }
 
         resizeFunc();
 
     } else {
         header.classList.remove('on');
-        sidebox.classList.remove('on');
-        sidebox.removeAttribute('style');
+        if (sidebox) {
+            sidebox.classList.remove('on');
+            sidebox.removeAttribute('style');
+        }
     }
 }
 
+// 새로고침 시, 첫 스크롤로 돌아가도록 설정 
+setTimeout(function() {
+    scrollTo(0, 0)
+}, 100)
 
-window.addEventListener('click', delegationFunc);
+if (delegation) {
+    delegation.addEventListener('click', delegationFunc);
+}
 window.addEventListener('resize', resizeFunc);
 window.addEventListener('scroll', scrollFunc);
